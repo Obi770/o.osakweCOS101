a77534ba6671efcecb6f1e6a1d51b6b983adcdd9
Obi770 <obichukwu.osakwe@pau.edu.ng>
2026-09-17 20:59:14 +0100

    Practices

---
+++ week-4/pratcice_2/target/debug/deps/pratcice_2.d
@@ -0,0 +1,5 @@
+C:\Users\HP\OneDrive\Documents\o.osakweCOS101\week-4\pratcice_2\target\debug\deps\pratcice_2.d: src\main.rs
+
+C:\Users\HP\OneDrive\Documents\o.osakweCOS101\week-4\pratcice_2\target\debug\deps\pratcice_2.exe: src\main.rs
+
+src\main.rs:
---
+++ week-4/pratcice_2/target/debug/pratcice_2.d
@@ -0,0 +1,1 @@
+C:\Users\HP\OneDrive\Documents\o.osakweCOS101\week-4\pratcice_2\target\debug\pratcice_2.exe: C:\Users\HP\OneDrive\Documents\o.osakweCOS101\week-4\pratcice_2\src\main.rs
